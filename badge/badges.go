99d96087038e916c51e7c26bd20f4209657bd74a
Jonas Plum <cugu@users.noreply.github.com>
2020-12-15 18:36:32 +0100

    Add owner and criticality (#15)

--- badge/badges.go
+++ badge/badges.go
@@ -27,19 +27,20 @@ func (b *Badge) ToMarkdown() string {
 }
 
 type Project struct {
-	Hoster            string   `yaml:"-"`
-	Namespace         string   `yaml:"-"`
-	Name              string   `yaml:"-"`
-	AzureOrganization string   `yaml:"azure-organization,omitempty"`
-	AzureProject      string   `yaml:"azure-project,omitempty"`
-	AzureDefinitionID string   `yaml:"azure-definition-id,omitempty"`
-	GoImportPath      string   `yaml:"goimportpath,omitempty"`
-	Workflow          string   `yaml:"workflow,omitempty"`
-	URL               string   `yaml:"url,omitempty"`
-	Disable           []string `yaml:"disable,omitempty"`
-	Enable            []string `yaml:"enable,omitempty"`
-	Token             string   `yaml:"token,omitempty"`
-	IsGitlab          bool     `yaml:"gitlab,omitempty"`
+	Hoster            string            `yaml:"-"`
+	Namespace         string            `yaml:"-"`
+	Name              string            `yaml:"-"`
+	Meta              map[string]string `yaml:"meta,omitempty"`
+	AzureOrganization string            `yaml:"azure-organization,omitempty"`
+	AzureProject      string            `yaml:"azure-project,omitempty"`
+	AzureDefinitionID string            `yaml:"azure-definition-id,omitempty"`
+	GoImportPath      string            `yaml:"goimportpath,omitempty"`
+	Workflow          string            `yaml:"workflow,omitempty"`
+	URL               string            `yaml:"url,omitempty"`
+	Disable           []string          `yaml:"disable,omitempty"`
+	Enable            []string          `yaml:"enable,omitempty"`
+	Token             string            `yaml:"token,omitempty"`
+	IsGitlab          bool              `yaml:"gitlab,omitempty"`
 }
 
 type badgeCreation func(Project) *Badge
--- badge/defaultbadges.go
+++ badge/defaultbadges.go
@@ -1,5 +1,7 @@
 package badge
 
+import "github.com/narqo/go-badge"
+
 func InitDefaultBadges() {
 	badges["icon"] = icon
 	badges["travis"] = markdownBadge("https://travis-ci.org/{{.Namespace}}/{{.Name}}.svg?branch=master", "https://travis-ci.org/{{.Namespace}}/{{.Name}}", nil)
@@ -7,6 +9,8 @@ func InitDefaultBadges() {
 	badges["codecov"] = markdownBadge("https://codecov.io/gh/{{.Namespace}}/{{.Name}}/branch/master/graph/badge.svg", "https://codecov.io/gh/{{.Namespace}}/{{.Name}}", nil)
 	badges["goreportcard"] = markdownBadge("https://goreportcard.com/badge/{{.GoImportPath}}", "https://goreportcard.com/report/{{.GoImportPath}}", nil)
 	badges["godoc"] = markdownBadge("https://godoc.org/{{.GoImportPath}}?status.svg", "https://godoc.org/{{.GoImportPath}}", nil)
+	badges["owner"] = owner
+	badges["criticality"] = criticality
 }
 
 func icon(project Project) *Badge {
@@ -18,4 +22,26 @@ func icon(project Project) *Badge {
 	default:
 		return nil
 	}
+}
+
+func owner(project Project) *Badge {
+	if owner, ok := project.Meta["owner"]; ok {
+		svgBadge(project.Hoster, project.Name, "owner", "owner", owner, badge.ColorBlue, project.URL, nil)
+	}
+	return svgBadge(project.Hoster, project.Name, "owner", "owner", "unknown", badge.ColorRed, project.URL, nil)
+}
+
+func criticality(project Project) *Badge {
+	criticality := "low"
+	color := badge.ColorBrightgreen
+	if c, ok := project.Meta["criticality"]; ok {
+		criticality = c
+		switch c {
+		case "critical":
+			color = badge.ColorRed
+		case "moderate":
+			color = badge.ColorOrange
+		}
+	}
+	return svgBadge(project.Hoster, project.Name, "criticality", "criticality", criticality, color, project.URL, nil)
 }
